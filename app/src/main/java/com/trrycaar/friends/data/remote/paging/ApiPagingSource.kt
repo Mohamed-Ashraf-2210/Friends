e4cc0a830d4666e88464898052ff77d22ad97952
Mohamed Ashraf <ma1261327@gmail.com>
2025-12-11 14:58:07 +0200

    refactor api paging source

--- app/src/main/java/com/trrycaar/friends/data/remote/paging/ApiPagingSource.kt
+++ app/src/main/java/com/trrycaar/friends/data/remote/paging/ApiPagingSource.kt
@@ -5,20 +5,20 @@ import androidx.paging.PagingState
 import com.trrycaar.friends.data.util.mapToDomainException
 
 class ApiPagingSource<T : Any>(
-    private val getDataFromApi: suspend (Int, pageSize: Int) -> List<T>,
-    private val pageSize: Int = 10
+    private val getDataFromApi: suspend (Int, pageSize: Int) -> List<T>
 ) : PagingSource<Int, T>() {
     override fun getRefreshKey(state: PagingState<Int, T>): Int? {
         return state.anchorPosition?.let { anchorPosition ->
-            state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
-                ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
+            val page = state.closestPageToPosition(anchorPosition)
+            page?.prevKey?.plus(1)
+                ?: page?.nextKey?.minus(1)
         }
     }
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, T> {
         val page = params.key ?: 1
         return try {
-            val data = getDataFromApi(page, pageSize)
+            val data = getDataFromApi(page, params.loadSize)
             LoadResult.Page(
                 data = data,
                 prevKey = if (page == 1) null else page - 1,
